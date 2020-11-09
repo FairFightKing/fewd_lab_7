3d2d55ae7b7b74ac48ea08c1e6e2d6f164ba8afe
FairFightKing <sosthenefruchard@gmail.com>
2020-11-09 14:17:05 +0000

    finished

---
+++ script/script.js
@@ -0,0 +1,31 @@
+ let firstName = "Sosthene";
+
+ let favouriteColor = "Pink";
+
+ let favouriteFood = "Bacon strips";
+
+ let city = "Voisins le Bx";
+// This line displays the firstname favouritecolor and favoritefood in the console of the browser
+ console.log(firstName + ' ' + favouriteColor + ' ' + favouriteFood);
+
+ let phrase, endingPhrase, one, two, three, four, five, six, seven;
+
+ phrase = "A rainbow is formed by the sunlight. Shining through rain drops so small. The rain bends the light from the sunshine, and colors the sky for us all.";
+ endingPhrase = "is what we see. Rainbow colours, are made with light bending you see!";
+ one = "Red";
+ two = "orange";
+ three = "yellow";
+ four = "green";
+ five = "blue";
+ six = "indigo";
+ seven = "violet";
+
+ console.log(`${phrase} ${one}, ${two}, ${three}, ${four}, ${five}, ${six}, ${seven} ${endingPhrase}`);
+
+ var myAlphabet = ['A', 'B', 'C', 'D','E','F', 'G'];
+
+ let myAlphabetLength = (Array) => {
+  console.log(Array.length)
+ };
+
+ myAlphabetLength(myAlphabet);
